0f82a01b54b754c766dfdb1926b29d265a53c36a
dejourford <dejourford@my.unt.edu>
2023-05-16 13:31:04 -0500

    fix issue with state functions

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,17 +1,24 @@
 import "./navigation.css"
+import React from "react"
+import { useState } from "react"
+
 
 export default function Navigation() {
+    const [isOpen, setIsOpen] = React.useState(false)
+    
     const closeMenu = () => {
         console.log('closed')
+        setIsOpen(prevState => !prevState)
     }
     
     const showMenu = () => {
         console.log('opened')
+        setIsOpen(prevState => !prevState)
     }
 
     return (
         <nav>
-            <div className="mobileNav">
+            <div className={isOpen? "mobileNavActive" : "mobileNav"}>
                 <button className="closeNavBtn" onClick={closeMenu}>
                    <i className="fa-solid fa-xmark"></i>
                 </button>
@@ -30,34 +37,41 @@ export default function Navigation() {
                     <li><a href="">Services</a></li>
                 </ul>
 
-                <button className="applyBtn">
+                <button className="applyBtn mobileApplyBtn">
                     <a href="">Apply Now</a>
                 </button>
             </div>
 
-            <div className="main-nav">
+            {/* main navigation for page */}
+            <div className= {isOpen? "mainNavInactive" : "mainNav"}>
             
-                <button className="openNavBtn" onClick={showMenu}>
-                    <i className="fa-solid fa-bars-sort"></i>
-                </button>
-                
-                
                 <div className="logo">
                     <i className="fa-solid fa-credit-card fa-rotate-270"></i>
                     <p className="logoText">BLKcard</p>
                 </div>
-                
-                <ul className="mobile-nav-links">
+
+                <button className="openNavBtn" onClick={showMenu}>
+                    <i className="fa-solid fa-bars"></i>
+                </button>
+
+                <ul className="main-nav-links">
                     <li><a href="">Features</a></li>
+                    <span>|</span>
                     <li><a href="">Home</a></li>
+                    <span>|</span>
                     <li><a href="">Payments</a></li>
+                    <span>|</span>
                     <li><a href="">Pricing</a></li>
+                    <span>|</span>
                     <li><a href="">Services</a></li>
+                    
+                    
                 </ul>
 
-                <button className="applyBtn">
+                <button className="applyBtn mainApplyBtn">
                     <a href="">Apply Now</a>
                 </button>
+                
            
             </div>
         </nav>
