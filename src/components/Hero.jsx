68d9a2deea7104fcf5ddbdbf3d36536df290ca9b
dejourford <dejourford@my.unt.edu>
2023-05-18 22:40:37 -0500

    add hero text and button group

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,10 +2,20 @@ import "./hero.css"
 import heroImage from "../assets/images/card-image.png"
 
 export default function Hero() {
+    const rightArrow = '\u2192'
+    
     return (
         <div className="heroSection">
             <div className="heroLeft">
-                <h1>wtf</h1>
+                <h1>The revolutionary BLK card is finally here.</h1>
+                <p className="heroText">Apply now and become one of the very few to witness 
+                    the power of BLK. <span className="heroTextEmphasis">*while supplies last*</span>
+                </p>
+                <div className="buttonGroup">
+                    <button className="applyBtn">Apply Now</button>
+                    <p className="learnMoreText">Learn More {rightArrow}</p>
+                </div>
+                
             </div>
 
             <div className="heroRight">
