9fa8485b456175c50fbcab8beea654833c88b6f8
dejourford <dejourford@my.unt.edu>
2023-05-19 08:15:59 -0500

    removed broken page link

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
-import "./home.css"
 import Navigation from "../components/Navigation"
 import Hero from "../components/Hero"
+import Features from "../components/Features"
+
 
 export default function Home() {
     return (
         <div className="container">
             <Navigation />
             <Hero />
+            <Features />
         </div>
     )
 }
