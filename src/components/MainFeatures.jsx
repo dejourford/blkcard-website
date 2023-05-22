c2611aa7e0da8cc743439718d96dfcb01471c595
dejourford <dejourford@my.unt.edu>
2023-05-22 01:13:25 -0500

    fix styling issues with media queries

--- src/components/MainFeatures.jsx
+++ src/components/MainFeatures.jsx
@@ -1,5 +1,9 @@
 import "./mainfeatures.css"
-import BalanceFeature from "./BalanceFeature"
+import transactionImg from "../assets/images/transaction-history.jpg"
+import expenseImg from "../assets/images/expense-tracker.jpg"
+
+
+
 export default function MainFeatures() {
     return (
         <section className="mainFeatureSection">
@@ -8,8 +12,30 @@ export default function MainFeatures() {
                 
             </div>
             
-            {/* insert feature component here */}
-            <BalanceFeature />
+            <section className="balanceFeature">
+                <div className="balanceFeatureLeft">
+                    <h3 className="balanceFeatureHeaderText">Seamless & Instant Balance Reflection</h3>
+                    <p className="balanceFeatureText">Get your current balance as soon as a transaction 
+                    is completed. No more waiting for transactions to post before your actual balance is reflected</p>
+                    <button className="applyBtn">Learn More</button>
+                </div>
+                <div className="balanceFeatureRight">
+                    <img className="transactionImg" src={transactionImg} alt="balance image" />
+                </div>
+            </section>
+
+            <section className="expenseFeature">
+                <div className="expenseFeatureLeft">
+                    <h3 className="expenseFeatureHeaderText">Seamless & Instant Balance Reflection</h3>
+                    <p className="expenseFeatureText">Get your current balance as soon as a transaction 
+                    is completed. No more waiting for transactions to post before your actual balance is reflected</p>
+                    <button className="applyBtn">Learn More</button>
+                </div>
+                <div className="expenseFeatureRight">
+                    <img className="expenseImg" src={expenseImg} alt="balance image" />
+                </div>
+            </section>
+            
         </section>
     )
 }
