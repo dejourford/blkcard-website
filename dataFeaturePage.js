ef51cdc5767763b505e445fc0799ae45055b98d2
dejourford <dejourford@my.unt.edu>
2023-05-30 12:55:45 -0500

    changed card titles

--- dataFeaturePage.js
+++ dataFeaturePage.js
@@ -7,7 +7,7 @@ import bitcoin from "./src/assets/images/bitcoin.jpg"
 export default [
     {
         id: 1,
-        title: ' Instant Balance Reflection',
+        title: 'Balance Reflection',
         text: `Get your current balance as soon as a transaction 
         is completed. No more waiting for transactions to post before your actual balance is reflected`,
         img: transactionImg
@@ -15,7 +15,7 @@ export default [
 
     {
         id: 2,
-        title: 'Instant Transaction Tracker',
+        title: 'Transaction Tracker',
         text: `Access every transaction you've ever made 
         as a BLKcard holder. We never delete transaction history so you can have
         peace of mind and added security all the time`,
