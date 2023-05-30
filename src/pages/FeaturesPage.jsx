38234c45817b439ae2097303a0978f966448489a
dejourford <dejourford@my.unt.edu>
2023-05-30 11:31:02 -0500

    added footer to Features Page

--- src/pages/FeaturesPage.jsx
+++ src/pages/FeaturesPage.jsx
@@ -1,10 +1,11 @@
 import Navigation from "../components/Navigation"
-
+import Footer from "../components/Footer"
 
 export default function FeaturesPage() {
     return (
         <div className="container">
             <Navigation />
+            <Footer />
             
         </div>
     )
