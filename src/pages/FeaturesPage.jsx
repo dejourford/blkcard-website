567d6819f5fe2a84b866c59d411860070a932e91
dejourford <dejourford@my.unt.edu>
2023-05-30 11:29:47 -0500

    added navigation to featurespage

---
+++ src/pages/FeaturesPage.jsx
@@ -0,0 +1,11 @@
+import Navigation from "../components/Navigation"
+
+
+export default function FeaturesPage() {
+    return (
+        <div className="container">
+            <Navigation />
+            
+        </div>
+    )
+}
