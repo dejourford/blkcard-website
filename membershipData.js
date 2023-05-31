adac9912291e0fc42bc0fa1e0fa95f7930d051b6
dejourford <dejourford@my.unt.edu>
2023-05-31 10:13:09 -0500

    add data

---
+++ membershipData.js
@@ -0,0 +1,32 @@
+import StarImage from "./src/assets/images/star.png"
+
+export default [
+    {
+        id: 1,
+        stars: StarImage,
+        rating: '2.9 (56)',
+        title: 'Silver',
+        description: `Choose our Silver membership which grants access to 
+        banks on the outside as well as the inside. It's great for people new to the industry. Also, you will be issued 
+        a plastic card.`,
+    },
+
+    {
+        id: 2,
+        stars: StarImage,
+        rating: '3.3 (108)',
+        title: 'Gold',
+        description: `Choose our Gold membership which includes all perks from Silver but also 
+        grants access to banks up to 50 miles outside of your local area. Also, you will be issued an aluminum
+        card.`,
+    },
+
+    {
+        id: 3,
+        stars: StarImage,
+        rating: '4.6 (324)',
+        title: 'Platinum',
+        description: `Choose our Platinum membership which includes all perks from Silver and Gold but also 
+        grants access to any bank nationwide. Also, you will be issued a 100% alloy metal card.`,
+    },
+]
