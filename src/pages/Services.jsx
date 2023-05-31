c351ac15e96e64344944aa7aa13fbc587396d5b7
dejourford <dejourford@my.unt.edu>
2023-05-31 11:25:04 -0500

    create services page

---
+++ src/pages/Services.jsx
@@ -0,0 +1,12 @@
+import Navigation from "../components/Navigation";
+import Footer from "../components/Footer";
+
+export default function Services() {
+    return (
+        <div className="container">
+           <Navigation />
+            <Footer /> 
+        </div>
+        
+    )
+}
