9b3c37ff10e1c7839df760d6ba3244c4d5d57753
dejourford <dejourford@my.unt.edu>
2023-05-31 10:53:53 -0500

    added wrapper to card

--- src/components/MembershipCard.jsx
+++ src/components/MembershipCard.jsx
@@ -2,17 +2,20 @@ import "./membershipcard.css"
 
 export default function MembershipCard(props) {
     return (
-        <div className="membershipCardWrapper">
-            <div className="membershipCardHeader">
-                <img className="stars" src={props.stars} alt="" />
-                <p className="rating">{props.rating}</p>
+        <div className="membershipCard">
+            <div className="membershipCardWrapper">
+                <div className="membershipCardHeader">
+                    <img className="stars" src={props.stars} alt="" />
+                    <p className="rating">{props.rating}</p>
+                </div>
+
+                <h1 className="membershipCardTitle">{props.title}</h1>
+
+                <p className="membershipCardDescription">{props.description}</p>
+
+                <button className="applyBtn membershipapplyBtn">Learn More</button>
             </div>
-
-            <h1 className="membershipCardTitle">{props.title}</h1>
-
-            <p className="membershipCardDescription">{props.description}</p>
-
-            <button className="applyBtn membershipapplyBtn">Learn More</button>
         </div>
+        
     )
 }
