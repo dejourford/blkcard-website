94bc2299c179a68aae2ff6329a950bfe58423104
dejourford <dejourford@my.unt.edu>
2023-05-31 10:13:58 -0500

    add map function for creating cards from data

--- src/pages/Memberships.jsx
+++ src/pages/Memberships.jsx
@@ -1,10 +1,21 @@
 import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
+import MembershipCard from "../components/MembershipCard";
+import membershipData from "../../membershipData";
 
 export default function Payments() {
+    const membershipCardElement = membershipData.map(function(item) {
+        return <MembershipCard key={item.id} stars={item.stars} rating={item.rating} title={item.title} description={item.description} />
+    })
+    
+    
     return (
         <div className="container">
             <Navigation />
+            <div className="membershipCardsSection">
+                {membershipCardElement}
+            
+            </div>
             <Footer />
         </div>
         
